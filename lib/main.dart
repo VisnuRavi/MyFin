3bf58a41348bedcf9e8d5c4e44cee34bd8c8589b
VisnuRavi <visnu9016@gmail.com>
2021-12-21 00:27:25 +0800

    Create basic invest page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 // ignore_for_file: prefer_const_constructors
 import "package:flutter/material.dart";
 import 'pages/home.dart';
+import 'pages/invest.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  routes: {
+    '/': (context) => Home(),
+    '/invest': (context) => Invest(),
+  },
 ));
---
+++ lib/pages/invest.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class Invest extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Investments',
+          style: TextStyle(
+          fontSize: 20.0,
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget> [
+          
+        ],
+      ),
+    );
+  }
+}
